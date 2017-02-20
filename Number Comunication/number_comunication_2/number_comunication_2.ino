97bf4dfe168ccbbe376fbdafc7901a3a3eed1fcc
StevenBermeo <steven1997unki@hotmail.com>
2017-02-20 15:47:42 +0100

    kevin

--- Number Comunication/number_comunication_2/number_comunication_2.ino
+++ Number Comunication/number_comunication_2/number_comunication_2.ino
@@ -10,24 +10,34 @@
 
 //********** Variables ********************************************************
 long n;
-
+long r;
 //********** Setup ************************************************************
 void setup() {
   Serial.begin(9600);        // initialize serial
-  Serial.println("Entra un número?");
+  Serial.println("Entra un numero?");
 }
 //********** Loop *************************************************************
-void loop() {
-  while (Serial.available() > 0) {                                  // Si hay una libreía disponible, procedera a leerla
+void loop()
+{
+  while (Serial.available() > 0) 
+  {                                  // Si hay una libreía disponible, procedera a leerla
     n = Serial.parseInt();                                         // Buscara un valor válido en el Stream, que es la base de datos para binarios.
       Serial.print ("El ");
-      Serial.print (n);
-      Serial.print  ("es ");                                   //Muestra el valor de r1
-      r=n/2
-    if 
-    if (Serial.read() == '\n')                                       //Una vez leido el serial agrega una linea nueva
-    { //look for newline. Is the end of your sentence
-      Serial.println("Entra nous valors per r1 i r2");      //mostra el resultats i fa de nou la pregunta inicial)
+      Serial.print (n);                                            //Muestra el valor que le introduzcamos
+      Serial.print  (" es ");                           
+     r=n%2;
+     
+  if ( r == 0 )
+  {
+      Serial.println  ("parell.");
+  }
+  else 
+  {
+    Serial.println ("senar");
+  }
+   if (Serial.read() == '\n')                                       //Una vez leido el serial agrega una linea nueva
+    {                                                         //look for newline. Is the end of your sentence
+      Serial.println("Entra un numero?");      //mostra el resultats i fa de nou la pregunta inicial)
     }
   }
-}
+  }
